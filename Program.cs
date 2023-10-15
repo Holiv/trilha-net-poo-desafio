8bfe90db78b0839f2ce950667452b42cbbd59434
holiv <mrheltonso@gmail.com>
2023-10-14 21:40:26 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("999874566", "2280", "123456", 128);
+Iphone iphone = new("996209854", "2280", "123456", 64);
+
+nokia.InstalarAplicativo("Nokia Tracker");
+iphone.InstalarAplicativo("MyAppleTracker");
